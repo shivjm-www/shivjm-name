4d85a7d2048580a8ded12d4e032cf9690db11505
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-10-28 00:57:45 +0530

    fix: make Eleventy copy index.css

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,6 @@ const tinyCSS = require("@sardine/eleventy-plugin-tinycss");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addPassthroughCopy("index.css");
   eleventyConfig.addPlugin(tinyCSS);
 };
