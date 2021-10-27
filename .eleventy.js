12f9a0e4bc603132f25a13b85e63f2eb83dc1b01
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-10-28 00:46:02 +0530

    feat: add page

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+const yaml = require("js-yaml");
+const tinyCSS = require("@sardine/eleventy-plugin-tinycss");
+
+module.exports = eleventyConfig => {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addPlugin(tinyCSS);
+};
